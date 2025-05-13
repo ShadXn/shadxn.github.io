0b807a6c998129a521345605beb631ae90039e4f
ShadXn <shadxn@sayver.net>
2025-05-13 19:34:51 +0200

    Update dailyclues.js

--- dailyclues.js
+++ dailyclues.js
@@ -75,11 +75,11 @@ function renderCards() {
 
   const totalDisplay1 = document.createElement('div');
   totalDisplay1.className = 'alert alert-info fw-bold text-center';
-  totalDisplay1.textContent = `Total Clues Completed: ${totalCluesDone} / ${totalTargetClues} (${completed.easy} Easy | ${completed.medium} Medium | ${completed.hard} Hard)`;
+  totalDisplay1.textContent = `Total Clues Completed: ${totalCluesDone} / ${totalTargetClues} (${startingClueCount.easy + completed.easy} Easy | ${startingClueCount.medium + completed.medium} Medium | ${startingClueCount.hard + completed.hard} Hard)`;
 
   const totalDisplay2 = document.createElement('div');
   totalDisplay2.className = 'alert alert-info fw-bold text-center';
-  totalDisplay2.textContent = `Total Clues Left: ${cluesLeft} (${clueTargets.easy - completed.easy} Easy | ${clueTargets.medium - completed.medium} Medium | ${clueTargets.hard - completed.hard} Hard)`;
+  totalDisplay2.textContent = `Total Clues Left: ${cluesLeft} (${clueTargets.easy - (startingClueCount.easy + completed.easy)} Easy | ${clueTargets.medium - (startingClueCount.medium + completed.medium)} Medium | ${clueTargets.hard - (startingClueCount.hard + completed.hard)} Hard)`;
 
   summaryContainer.appendChild(totalDisplay1);
   summaryContainer.appendChild(totalDisplay2);
