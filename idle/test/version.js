08f72d2d59ace086b46f431a214eb7092b8cee8e
ShadXn <shadxn@sayver.net>
2025-07-04 06:38:28 +0200

    d

--- idle/test/version.js
+++ idle/test/version.js
@@ -2,9 +2,6 @@
 const CURRENT_GAME_VERSION = "0.21";
 const VERSION_STORAGE_KEY = "idle_last_version";
 
-localStorage.setItem("debug_junk1", "foo");
-localStorage.setItem("unused_feature_flag", "true");
-
 const LOCALSTORAGE_WHITELIST = [
   "idle_gold",
   "idle_workers",
@@ -12,7 +9,5 @@ const LOCALSTORAGE_WHITELIST = [
   "idle_assignments",
   "sidebar_preferences",
   "idle_last_version", // needed for version popup
-  "debug_junk1",
-  "unused_feature_flag",
   VERSION_STORAGE_KEY,
 ];
