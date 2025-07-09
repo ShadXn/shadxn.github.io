6d4f29058cef11a993e43c85b59c63659037b00a
ShadXn <shadxn@sayver.net>
2025-07-09 13:32:54 +0200

    Update idlegame.js

--- idle/test/idlegame.js
+++ idle/test/idlegame.js
@@ -122,9 +122,7 @@
         if (
             DEBUG_MODE ||
             window.location.hostname === "localhost" ||
-            window.location.hostname === "127.0.0.1" ||
-            window.location.hostname.startsWith("192.168.") || // Optional: LAN testing
-            window.location.hostname === "0.0.0.0"
+            window.location.hostname === "127.0.0.1"
             ) {
             // Insert dev panel HTML
             const panel = document.createElement("div");
